99803329b5e5a2b45123de49a1cda943a4bb2fbf
Josh <jtstalngs@gmail.com>
2022-01-30 16:20:23 -0600

    Moved my local folder onto github

---
+++ stallingsj22/assignment01/index.php
@@ -0,0 +1,93 @@
+<html lang="en">
+    <head>
+        <style> 
+        body {
+            background-color: #b5c1c4;
+        }
+        div {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-bottom: 1%;
+        }
+
+        table, th, td {
+            border: 1px solid #b5c1c4;
+            border-collapse: collapse;
+            text-align: center;
+        }
+        th, td {
+            background-color: #d1dbde;
+            padding: 15px;
+            height: 50px;
+            width: 50px;
+        } 
+        table {
+            box-shadow: 1px 1px 20px grey;
+        }
+        td:hover {
+            background-color: #b7c1c4;
+        }
+        button:hover{
+            background-color: #a0b3b8;
+        }
+        button {
+            background-color: #a8b3b5;
+            margin-right: 5px;
+            box-shadow: 1px 1px 20px grey;
+        }
+        a {
+            color: black;
+        }
+        </style>
+        <title>INFINITY COLUMNS</title>
+    </head>
+
+    <body>
+        <div>
+        <table> 
+        <?php
+
+            function science_notation($x) {
+                $i = 0;
+                while($x > 10) {
+                    $x /= 10;
+                    $i += 1;
+                }
+                $x = round($x, 3);
+
+                return $x."E+".$i;
+
+            }
+            $ROWS = 5;
+            $COLUMNS = 5;
+            $row_data = "";
+            if(isset($_GET["ROWS"])) $ROWS = $_GET["ROWS"];
+            if(isset($_GET["COLUMNS"])) $COLUMNS = $_GET["COLUMNS"];
+
+            for($i = 0; $i < $ROWS+1; $i++) { //+1 because  header row
+                echo "<tr>";
+                for($j = 0; $j < $COLUMNS+1; $j++) {
+                    $pow = pow($i, $j);
+                    $num = $pow >= pow(10, 6) ? science_notation($pow) : $pow;
+
+                    if ($i==0 & $j==0) echo "<td></td>";
+                    else if($i == 0 & $j > 0) echo "<th> {$j} </th>";
+                    else if($i > 0 & $j == 0) echo "<td><b> $i </b></td>";
+                    else echo "<td> ". $num ."</td>";
+                }
+                echo "</tr>";
+            }
+        ?>
+        </table>
+        </div>
+
+        <div>
+        <button type="button"><a href="columns.php?COLUMNS=<?=$COLUMNS+1?>&ROWS=<?=$ROWS?>">Add Column </a></button>
+        <button type="button"><a href="columns.php?COLUMNS=<?=$COLUMNS-1?>&ROWS=<?=$ROWS?>">Remove Column </a></button>
+        <button type="button"><a href="columns.php?COLUMNS=<?=$COLUMNS?>&ROWS=<?=$ROWS+1?>">Add Row </a></button>
+        <button type="button"><a href="columns.php?COLUMNS=<?=$COLUMNS?>&ROWS=<?=$ROWS-1?>">Remove Row </a></button>
+        </div>  
+    </body>
+    
+</html>
---
+++ stallingsj22/index.php
@@ -0,0 +1,47 @@
+
+<html>
+
+<head>
+    <style>
+        body {
+            font-family: sans-serif;
+            background-color: #bec9ca;
+            text-align: center;
+        }
+
+        /*    p {
+            background-color: slategrey;
+            width: 150px;
+            height: 25px;
+            padding-top: 5px;
+        } */
+
+        a:link,
+        a:visited {
+            background-color: #abb9ba;
+            color: black;
+            padding: 15px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            border-style: solid;
+            border-color: #93a7a6;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+    </style>
+    <link rel="icon" type="image/x-icon" href="/stallingsj22/favicon.icon">
+    <title>Home Page</title>
+</head>
+
+<div style="border-style:solid; border-color: #93a7a6; background-color: #e0e5e6;">
+    <h1>Joshua Stallings</h1>
+    <h2>Assignment Hub</h2>
+</div>
+
+<div align="center">
+    <p><a href="/stallingsj22/assignment00/index.html">Assignment 00</a></p>
+</div>
+</html>
